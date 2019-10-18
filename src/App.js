c595b5a70d1f71cdc2052e515c9d8e64b28860f1
cboydstun <cboydstun@users.noreply.github.com>
2019-10-18 15:46:25 -0500

    remove mode:no-cors

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`, { mode: 'no-cors'});
+    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
     console.log(data);
   }
