e9a871d2ea14d802eb2a80f74d73865002bf4874
cboydstun <cboydstun@users.noreply.github.com>
2019-10-18 15:44:08 -0500

    add {mode: no-cors}

--- src/App.js
+++ src/App.js
@@ -9,8 +9,13 @@ const API_KEY = "b2dd32ee92060ae3bb9e523370c35987";
 class App extends React.Component {
   
   getWeather = async (e) => {
+    
     e.preventDefault();
-    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`);
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+
+    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`, { mode: 'no-cors'});
     const data = await api_call.json();
     console.log(data);
   }
