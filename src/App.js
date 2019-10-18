f412e097384b476f463a97c300940da7faacf22b
cboydstun <cboydstun@users.noreply.github.com>
2019-10-18 17:27:25 -0500

    comment out data parse

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
 
     const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
-    const data = await api_call.json();
+    //const data = await api_call.json();
     console.log(data);
   }
 
