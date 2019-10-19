f853c4b547c23144814d68040f4ac16ed88dc9f8
Chris Boydstun <chrisboydstun@gmail.com>
2019-10-18 20:50:17 -0500

    add state and props

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ const API_KEY = "b2dd32ee92060ae3bb9e523370c35987";
 class App extends React.Component {
   
   getWeather = async (e) => {
+
+    state = {
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: undefined
+    }
     
     e.preventDefault();
 
@@ -18,6 +27,14 @@ class App extends React.Component {
     const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
     console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ''
+    });
   }
 
   render(){
@@ -25,7 +42,14 @@ class App extends React.Component {
       <div>
         <Titles />
         <Form getWeather={this.getWeather}/>
-        <Weather />
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
