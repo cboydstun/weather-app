71df7e0426673fb19d65ef61d07fc4ccb85931ed
Chris Boydstun <chrisboydstun@gmail.com>
2019-10-18 19:11:25 -0500

    removed API address

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    https://samples.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}
-
     const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
     const data = await api_call.json();
     console.log(data);
