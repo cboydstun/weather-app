b0440d927dc62ed69e7a8fcfe0d9f8d7782c25ff
Chris Boydstun <chrisboydstun@gmail.com>
2019-10-21 15:59:19 -0500

    reduced columns to five

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ class App extends React.Component {
                 <div className="col-xs-5 title-container">
                   <Titles />
                 </div>
-                <div className="col-xs-7 form-container">
+                <div className="col-xs-5 form-container">
                   <Form getWeather={this.getWeather} />
                   <Weather 
                     temperature={this.state.temperature} 
